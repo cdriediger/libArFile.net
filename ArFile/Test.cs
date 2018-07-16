3115db5f245f76053e5ad6350227c89c1c3c7eb4
Christoph Driediger <christoph@driediger.eu>
2018-07-16 15:47:39 +0200

    Added "AddFile" Test

--- ArFile/Test.cs
+++ ArFile/Test.cs
@@ -11,14 +11,15 @@ namespace ArFile
         static void Main()
         {
             AddFile();
+            //AddData();
             //ReadArchive();
             //WriteTest();
             Console.ReadLine();
         }
 
-        static public void AddFile()
+        static public void AddData()
         {
-            Archive archive = new Archive(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testarchive");
+            Archive archive = new Archive(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testarchive.arfile");
             int f1 = archive.NewFile(@"C:\tmp\test.txt");
             string data = "1234567890";
             int c1 = archive.Metadata.GetChunk(data.Length);
@@ -29,21 +30,39 @@ namespace ArFile
             archive.Dispose();
         }
 
-        static public void AddRandomData()
+        static public void AddFile()
         {
+            Archive archive = new Archive(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testarchive.arfile");
+            string filePath = @"C:\tmp\test.txt";
+            int fileId = archive.NewFile(filePath);
+            FileStream file = new FileStream(filePath, FileMode.Open);
 
+            while (!(file.Position == file.Length))
+            {
+                int dataLength = archive.Metadata.defaultChunkSize;
+                if (!(file.Length - file.Position >= dataLength))
+                {
+                    dataLength = unchecked((int)(file.Length - file.Position));
+                }
+                byte[] data = new byte[dataLength];
+                file.Read(data, 0, dataLength);
+                int chunkId = archive.Metadata.GetChunk(dataLength);
+                archive.WriteChunk(data, chunkId, fileId);
+            }
+            file.Close();
+            archive.Dispose();
         }
 
         static public void ReadArchive()
         {
-            Archive archive = new Archive(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testarchive");
+            Archive archive = new Archive(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testarchive.arfile");
             string data = archive.ReadChunk(3);
             Logger.Warn($"Data: '{data}'");
         }
 
         static public void WriteTest()
         {
-            FileStream ArchivFile = System.IO.File.Open(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testfile", FileMode.CreateNew, FileAccess.ReadWrite);
+            FileStream ArchivFile = System.IO.File.Open(@"C:\Users\c.driediger\source\repos\ArFile\ArFile\testarchive.arfile", FileMode.CreateNew, FileAccess.ReadWrite);
             ArchivFile.Seek(100, SeekOrigin.Begin);
             ArchivFile.Write(Encoding.ASCII.GetBytes("Data"), 0, 4);
             ArchivFile.Seek(0, SeekOrigin.Begin);
